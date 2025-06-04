46a458087212279d92fdabb31f3da371197de6f5
Suryabhan Singh <suryabhan283808@gmail.com>
2025-06-04 18:30:15 +0530

    feat: Implement application form with dynamic input handling

--- Cybrom/Hook/src/App.jsx
+++ Cybrom/Hook/src/App.jsx
@@ -70,24 +70,102 @@
 // }
 // export default App;
 
-import { useEffect, useState } from "react";
 
-const App=()=>{
 
-  const [count,setCount]=useState(0);
-  const [multi,setMulti]=useState(0);
 
-  useEffect(()=>{
-    setMulti(count*2);
-  })
-  return(
+
+// import { useEffect, useState } from "react";
+
+// const App=()=>{
+
+//   const [count,setCount]=useState(0);
+//   const [multi,setMulti]=useState(0);
+
+//   useEffect(()=>{
+//     setMulti(count*2);
+//   })
+//   return(
+//     <>
+// <h1>welcome my app</h1>
+// <button onClick={()=>{setCount(count+1)}}>click</button>
+// <h1>my count :{count}</h1>
+// <h1 >multipilication:{multi}</h1>
+//     </>
+//   )
+// } 
+
+// export default App;
+
+
+
+
+
+
+// form
+
+// import { useState } from "react";
+
+// const App = () => {
+//   const [name, setName] = useState("");
+//   const [city, setCity] = useState("");
+
+//   const handleSubmit = () => {
+//     console.log({ name: name, city: city }); 
+//   };
+
+//   return (
+//     <>
+
+//      <h1>Application Form</h1>
+//      Enter name : <input type="text" value={name}  onChange={(e) => {
+//     setName(e.target.value)
+//   }}/>
+//      <br />
+//      Enter city : <input type="text"  value={city}  onChange={(e) => {
+//     setCity(e.target.value)
+//   }} />
+//       <br />
+
+//       <button onClick={handleSubmit}>Click!</button> 
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+import { useState } from "react";
+
+const App = () => {
+  
+  const [input, setInput] = useState({});
+
+  const handleInput = (e) => {
+    let name=e.target.name;
+    let value=e.target.value;
+    setInput(value=>({...value, [name]:value}));
+  }
+
+  return (
     <>
-<h1>welcome my app</h1>
-<button onClick={()=>{setCount(count+1)}}>click</button>
-<h1>my count :{count}</h1>
-<h1 >multipilication:{multi}</h1>
+
+     <h1>Application Form</h1>
+     Enter Rollno: <input type="text" name="rollno" onChange={handleInput} />
+
+      <br />
+     Enter name : <input type="text" name="name"  onChange={handleInput} />
+     <br />
+     Enter city : <input type="text"  name="city"  onChange={handleInput} />
+      <br />
+     Enter fees : <input type="text"  name="fees"  onChange={handleInput} /> 
+
+      <button>Click!</button> 
     </>
-  )
-} 
+  );
+};
 
 export default App;
